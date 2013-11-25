a43524f3bc08b31f5a4c82e38d7a2e634c15540e
andrewleo <hz_liuxiao@corp.netease.com>
2013-11-25 17:11:20 +0800

    issue #10:Memory above 1000;Improvement:List the application with alphabet order

--- src/com/netease/qa/emmagee/utils/Programe.java
+++ src/com/netease/qa/emmagee/utils/Programe.java
@@ -16,6 +16,7 @@
  */
 package com.netease.qa.emmagee.utils;
 
+
 import android.graphics.drawable.Drawable;
 
 /**
@@ -24,7 +25,7 @@ import android.graphics.drawable.Drawable;
  * 
  * @author andrewleo
  */
-public class Programe {
+public class Programe implements Comparable<Programe>{
 	private Drawable icon;
 	private String processName;
 	private String packageName;
@@ -70,5 +71,10 @@ public class Programe {
 
 	public void setPid(int pid) {
 		this.pid = pid;
+	}
+
+	@Override
+	public int compareTo(Programe arg0) {
+		return (this.getProcessName().compareTo(arg0.getProcessName()));
 	}
 }
